5df139098b7420df438ebdfef4b413e8e67a856d
wynn.netherland@gmail.com <wynn.netherland@gmail.com>
2011-04-19 12:39:13 -0500

    project stub

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,34 +1,9 @@
-# Make sure you list all the project template files here in the manifest.
-discover :all
-
 description "Teach your forms some manners."
 
 help %Q{
-  Please see the Formalize website for documentation:
+  To install, use the formalize pattern
 
-      http://formalize.me/
-}
-
-welcome_message %Q{
-
-  Formalize assets have been copied to the proper asset folders
-  based on your Compass configuration.
-
-  Usage:
-
-   If you're using SCSS syntax, in your stylesheet:
-
-     @import "formalize";
-
-   or if you're using indented syntax
-
-     @import "formalize"
-
-   In your HTML markup, include the JavaScript implementation of
-   your choice:
-
-   // a jQuery example
-   <script src="/javascripts/jquery.formalize.js"></script>
+    compass install compass_formalize/formalize
 
   Please see the Formalize website for documentation:
 
