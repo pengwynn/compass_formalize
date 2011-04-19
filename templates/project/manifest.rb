896c1459a32a6afb43f652725c5aa7d3bf0d4b0a
wynn.netherland@gmail.com <wynn.netherland@gmail.com>
2011-04-19 12:56:23 -0500

    separate js lib templates

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,9 +1,15 @@
 description "Teach your forms some manners."
 
 help %Q{
-  To install, use the formalize pattern
+  To install, use the formalize pattern:
 
-    compass install compass_formalize/formalize
+  Ensure formalize is in your config.rb (this will be done for you automatically if you require it on project creation)
+
+    require 'compass_formalize'
+
+  Next run the formalize pattern and follow the instructions.
+
+    compass install formalize/formalize
 
   Please see the Formalize website for documentation:
 
