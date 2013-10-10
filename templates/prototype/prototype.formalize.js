44a8a0d0ed542fbbcdb2f58cf719041211ba8335
Mark Reilly <mark@alienresident.net>
2013-10-10 11:49:34 -0400

    Updated from javasscripts from formalize 1.1 to 1.2

--- templates/prototype/prototype.formalize.js
+++ templates/prototype/prototype.formalize.js
@@ -1,5 +1,5 @@
 /*
-  Formalize - version 1.1
+  Formalize - version 1.2
 
   Note: This file depends on the Prototype library.
 */
@@ -7,12 +7,6 @@
 // Module pattern:
 // http://yuiblog.com/blog/2007/06/12/module-pattern
 var FORMALIZE = (function(window, document, undefined) {
-  // Private constants.
-  var PLACEHOLDER_SUPPORTED = 'placeholder' in document.createElement('input');
-  var AUTOFOCUS_SUPPORTED = 'autofocus' in document.createElement('input');
-  var IE6 = IE(6);
-  var IE7 = IE(7);
-
   // Internet Explorer detection.
   function IE(version) {
     var b = document.createElement('b');
@@ -20,16 +14,35 @@ var FORMALIZE = (function(window, document, undefined) {
     return !!b.getElementsByTagName('br').length;
   }
 
+  // Private constants.
+  var PLACEHOLDER_SUPPORTED = 'placeholder' in document.createElement('input');
+  var AUTOFOCUS_SUPPORTED = 'autofocus' in document.createElement('input');
+  var IE6 = IE(6);
+  var IE7 = IE(7);
+
   // Expose innards of FORMALIZE.
   return {
     // FORMALIZE.go
     go: function() {
-      for (var i in FORMALIZE.init) {
-        FORMALIZE.init[i]();
+      var i, j = this.init;
+
+      for (i in j) {
+        j.hasOwnProperty(i) && j[i]();
       }
     },
     // FORMALIZE.init
     init: {
+      // FORMALIZE.init.disable_link_button
+      disable_link_button: function() {
+        $(document.documentElement).observe('click', function(ev) {
+          var el = ev.target;
+          var is_disabled = el.tagName.toLowerCase() === 'a' && el.className.match('button_disabled');
+
+          if (is_disabled) {
+            ev.preventDefault();
+          }
+        });
+      },
       // FORMALIZE.init.full_input_size
       full_input_size: function() {
         if (!IE7 || !$$('textarea, input.input_full').length) {
@@ -91,7 +104,11 @@ var FORMALIZE = (function(window, document, undefined) {
           return;
         }
 
-        $$('[autofocus]')[0].focus();
+        var el = $$('[autofocus]')[0];
+
+        if (!el.disabled) {
+          el.focus();
+        }
       },
       // FORMALIZE.init.placeholder
       placeholder: function() {
@@ -104,6 +121,12 @@ var FORMALIZE = (function(window, document, undefined) {
         FORMALIZE.misc.add_placeholder();
 
         $$('[placeholder]').each(function(el) {
+          // Placeholder obscured in older browsers,
+          // so there's no point adding to password.
+          if (el.type === 'password') {
+            return;
+          }
+
           var text = el.getAttribute('placeholder');
           var form = el.up('form');
 
@@ -144,6 +167,12 @@ var FORMALIZE = (function(window, document, undefined) {
         }
 
         $$('[placeholder]').each(function(el) {
+          // Placeholder obscured in older browsers,
+          // so there's no point adding to password.
+          if (el.type === 'password') {
+            return;
+          }
+
           var text = el.getAttribute('placeholder');
 
           if (!el.value || el.value === text) {
